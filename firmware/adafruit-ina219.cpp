886f040727103801fded1bdbd7ea66fe4b90750a
cyberlord8 <cyberlord@cyber-wizard.com>
2015-09-30 13:17:07 -0700

    Update adafruit-ina219.cpp

--- firmware/adafruit-ina219.cpp
+++ firmware/adafruit-ina219.cpp
@@ -18,15 +18,20 @@
     v1.0 - First release
 */
 /**************************************************************************/
-#if ARDUINO >= 100
- #include "Arduino.h"
+#if defined (SPARK)
+  #include "application.h"
 #else
- #include "WProgram.h"
+  #if defined(ARDUINO) && ARDUINO >= 100
+    #include "Arduino.h"
+  #else
+    #include "WProgram.h"
+  #endif
+  // here could follow some of the includes only needed on Arduino
+  // see bellow
+
 #endif
 
-#include <Wire.h>
-
-#include "Adafruit_INA219.h"
+#include "adafruit-ina219.h"
 
 /**************************************************************************/
 /*! 
