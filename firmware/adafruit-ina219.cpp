30f73ae8cd35b8e1b4fe97a2c9213ead221b7902
cyberlord8 <cyberlord@cyber-wizard.com>
2015-09-30 13:38:34 -0700

    Update adafruit-ina219.cpp

--- firmware/adafruit-ina219.cpp
+++ firmware/adafruit-ina219.cpp
@@ -46,6 +46,11 @@ void Adafruit_INA219::wireWriteRegister (uint8_t reg, uint16_t value)
     Wire.write((value >> 8) & 0xFF);       // Upper 8-bits
     Wire.write(value & 0xFF);              // Lower 8-bits
   #else
+  #ifdef SPARK
+    Wire.write(reg);                       // Register
+    Wire.write((value >> 8) & 0xFF);       // Upper 8-bits
+    Wire.write(value & 0xFF);              // Lower 8-bits
+  #else
     Wire.send(reg);                        // Register
     Wire.send(value >> 8);                 // Upper 8-bits
     Wire.send(value & 0xFF);               // Lower 8-bits
@@ -64,6 +69,9 @@ void Adafruit_INA219::wireReadRegister(uint8_t reg, uint16_t *value)
   Wire.beginTransmission(ina219_i2caddr);
   #if ARDUINO >= 100
     Wire.write(reg);                       // Register
+  #else
+  #ifdef SPARK
+    Wire.write(reg);
   #else
     Wire.send(reg);                        // Register
   #endif
